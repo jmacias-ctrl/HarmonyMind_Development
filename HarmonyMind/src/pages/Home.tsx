88de2b15fa70805a1f44771aed3c18769b7ec766
Rayen <rnahuelhual@ing.ucsc.cl>
2024-04-22 18:58:42 -0400

    moving some

---
+++ HarmonyMind/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import './Home.css';
+
+const Home: React.FC= () =>{
+    return(
+    <IonPage>
+        <IonContent className='ion-padding'>
+            <div className='circle1'>
+            </div>
+        </IonContent>
+    </IonPage>
+    
+    )
+}
+
+export default Home;
