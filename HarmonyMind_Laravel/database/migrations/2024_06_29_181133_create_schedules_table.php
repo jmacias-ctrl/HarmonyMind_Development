70826aa3812d5347eedc1ed9a665b788e2afac2b
m-madariaga <121448640+m-madariaga@users.noreply.github.com>
2024-06-29 19:20:52 -0400

    creación mograciones experto, horario y cita agendada

---
+++ HarmonyMind_Laravel/database/migrations/2024_06_29_181133_create_schedules_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('expert_fk')->references('id')->on('experts');
+            $table->time('hora', 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
